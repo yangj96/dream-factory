8868684e1ab4712b75a4afc1fee8f7c7e5a9e78c
yangj96 <yangj_96@126.com>
2020-01-15 19:52:51 +0800

    frontend homepage skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,104 @@
+import { Layout, Breadcrumb, Card, Col, Row, Collapse } from 'antd';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer } = Layout;
+const { Panel } = Collapse;
+
+class App extends React.Component<> {
+  render() {
+    return (
+      <Layout className="layout">
+        <Header>
+          <div className="logo">
+            <span>Dream Factory - Image Semantic Editing</span>
+          </div>
+        </Header>
+        <Content style={{padding: '0 25px'}}>
+          <Breadcrumb style={{margin: '16px 0'}}>
+          </Breadcrumb>
+          {/* select image block*/}
+          <Card title="Select a image and have a try!">
+            <Row gutter={12}>
+              <Col span={4}>
+                <Card t hoverable
+                      style={{ width: 180 }}
+                      bordered={false}
+                      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  Select this!
+                </Card>
+              </Col>
+              <Col span={4}>
+                <Card t hoverable
+                      style={{ width: 180 }}
+                      bordered={false}
+                      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  Select this!
+                </Card>
+              </Col>
+              <Col span={4}>
+                <Card t hoverable
+                      style={{ width: 180 }}
+                      bordered={false}
+                      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  Select this!
+                </Card>
+              </Col>
+              <Col span={4}>
+                <Card t hoverable
+                      style={{ width: 180 }}
+                      bordered={false}
+                      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  Select this!
+                </Card>
+              </Col>
+              <Col span={4}>
+                <Card t hoverable
+                      style={{ width: 180 }}
+                      bordered={false}
+                      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  Select this!
+                </Card>
+              </Col>
+              <Col span={4}>
+                <Card t hoverable
+                      style={{ width: 180 }}
+                      bordered={false}
+                      cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                  Select this!
+                </Card>
+              </Col>
+            </Row>
+          </Card>
+          <br />
+          {/* Multiple Transform*/}
+          <Collapse defaultActiveKey={['1']}>
+            <Panel header="Emotion Transform" key="1">
+
+            </Panel>
+            <Panel header="Smile Transform" key="2">
+
+            </Panel>
+            <Panel header="Age Transform" key="3">
+
+            </Panel>
+            <Panel header="Sex Transform" key="4">
+
+            </Panel>
+          </Collapse>
+        </Content>
+        <Footer style={{textAlign: 'center'}}>
+          Dream Factory ©2020 Created by Google Winter ML Camp X.Y.Z Team
+        </Footer>
+      </Layout>
+    );
+  }
 }
 
 export default App;
